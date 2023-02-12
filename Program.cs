22b2dfb3df7c26fdc9f9b12657fa8c7b332f87ce
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-02-12 18:20:34 -0300

    calculates the age's media

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string firstPerson, secondPerson;
+int ageOne, ageTwo;
+double media;
+
+Console.WriteLine("Digite o nome da primeira pessoa");
+firstPerson = Console.ReadLine();
+
+Console.WriteLine("Digite o nome da segunda pessoa");
+secondPerson = Console.ReadLine();
+
+Console.WriteLine("Digite a idade da primeira pessoa");
+ageOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a idade da segunda pessoa");
+ageTwo = int.Parse(Console.ReadLine());
+
+media = (ageOne + ageTwo) / 2;
+
+Console.WriteLine($"A idade média de {firstPerson} e {secondPerson} é de " + media + " anos");
+
